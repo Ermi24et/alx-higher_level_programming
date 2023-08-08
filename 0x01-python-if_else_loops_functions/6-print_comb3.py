16b4064b65aaf042cb54d7ad74484e4ad58d4578
Ermi24et <lilermi24@gmail.com>
2023-08-08 06:26:17 -0700

    printing two digits without repeating

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i != 8 and j != 9:
+        if i + j != 17:
             print('{}{}'.format(str(i), str(j)), end=", ")
         if i == 8 and j == 9:
             print('{}{}'.format(i, j))
-
