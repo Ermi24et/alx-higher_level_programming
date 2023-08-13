ad2594c1d43c10940842e98e4f08c3d8b5ca846a
Ermi24et <lilermi24@gmail.com>
2023-08-13 02:05:51 -0700

    printing lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print('{}'.format(num))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for num in my_list:
+            print('{}'.format(num))
