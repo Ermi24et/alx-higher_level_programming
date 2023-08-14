616eae112affacc991736d819b2a0cb3d5d5c7e4
Ermi24et <lilermi24@gmail.com>
2023-08-14 03:44:34 -0700

    divisible by 2 or not

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
-    for i in my_list:
-        if i % 2 == 0:
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
             new_list[i] = 1
         else:
             new_list[i] = 0
