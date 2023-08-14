4d321f3a88a902844fa2af3dbf8b81513c1fba04
Ermi24et <lilermi24@gmail.com>
2023-08-14 02:17:57 -0700

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a = (), tuple_b = ()):
+def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a)) < 1:
         tuple_a = (0, 0)
     elif (len(tuple_a)) < 2:
