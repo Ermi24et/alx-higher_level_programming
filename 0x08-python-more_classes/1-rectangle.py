48247d565b399fc001d5e47da6a099f66b3bbec0
Ermi24et <lilermi24@gmail.com>
2023-09-04 09:14:57 -0700

    defining a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" creating a rectangle class """
+
+
+class Rectangle:
+    """ defining a rectangle with private instances """
+    def __init__(self, width=0, height=0):
+        """ instatinating """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getting width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting width """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getting height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setting height """
+        if type(value) != int:
+            raise ValueError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
