1298a1458623aa518413056e29af6758212314a4
Ermi24et <lilermi24@gmail.com>
2023-09-04 14:41:03 -0700

    adding the del function

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" a rectangle class that defines a rectangle """
+
+
+class Rectangle:
+    """ a class that adds public class attribute to the prev """
+    def __init__(self, width=0, height=0):
+        """ instanition """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ gettimg width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getting height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ returns the area of a rectangle """
+        return self.__height * self.width
+
+    def perimeter(self):
+        """ returns teh perimeter of a rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """ prints the rectangle with the char '#'"""
+        if self.__width == 0 or self.height == 0:
+            return ""
+        return ("\n".join(('#' * self.__width for i in range(self.__height))))
+
+    def __repr__(self):
+        """ returns a string to recreate new instance by using eval() """
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        """ prints bye rectangle... when an istance of rectange deleted """
+        print("bye rectangle...")
