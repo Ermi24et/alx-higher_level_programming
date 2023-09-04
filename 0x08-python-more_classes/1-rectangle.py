9b1763b2c323be154e2236af426cd68d12efd1e6
Ermi24et <lilermi24@gmail.com>
2023-09-04 09:54:57 -0700

    defining a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self, value):
         """ setting height """
         if type(value) is not int:
-            raise ValueError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
