94ccdcc070d2cd075f58f8908181ad211c526341
Ermi24et <lilermi24@gmail.com>
2023-09-11 14:09:36 -0700

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ a module that contains the class MyList
 
 class MyList(list):
     """ subclass of a lsit """
-    
+
     def print_sorted(self):
         """ prints a sorted list """
         print(sorted(self))
