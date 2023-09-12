4f41639d35b609f2dd6b7b503bbe7acc9dd2d2c0
Ermi24et <lilermi24@gmail.com>
2023-09-11 17:11:31 -0700

    base geometry continued

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" contains the class BaseGeometry """
+
+
+class BaseGeometry:
+    """a class with instance methods area and integer_validator"""
+    def area(self):
+        """ raises an exception when called """
+        raise Exception ("area() is not implemnted")
+
+    def integer_validator(self, name, value):
+        """ validates that value is an integer greater than zero"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
