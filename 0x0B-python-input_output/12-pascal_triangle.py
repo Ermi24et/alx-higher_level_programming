6e0feb08661a9d718319eb7c4bc8c2b2872df87a
Ermi24et <lilermi24@gmail.com>
2023-09-12 15:05:40 -0700

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" defines a pascal triangle rule """
+
+
+def pascal_triangle(n):
+    """ returns a list of list of integers """
+    
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
