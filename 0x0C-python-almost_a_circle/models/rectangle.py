f29da3be0e54cb2009790002e211b37512a4225f
Ermi24et <lilermi24@gmail.com>
2023-09-16 11:03:20 -0700

    rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,8 @@ class Rectangle(Base):
         else:
             for e, f in kwargs.items():
                 self.__setattr__(e, f)
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a rectangle """
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
