ad0c7891c0a09a8457e9dededc849b9251b755a3
Ermi24et <lilermi24@gmail.com>
2023-10-18 19:15:22 -0700

    only comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,5 +4,5 @@ FROM tv_shows INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 	INNER JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.name = 'Comedy'
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
