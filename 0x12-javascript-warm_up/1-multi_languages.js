bdbffb287065ea1d82fdb0f58c4578d1b0816a8d
Ermi24et <lilermi24@gmail.com>
2023-11-13 01:45:12 -0800

    3 fun languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints 3 lines of text
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+console.log(c);
+console.log(p);
+console.log(j);
