1728b7ec4134278b34aa802c8df44f32d9eaaf69
Ermi24et <lilermi24@gmail.com>
2023-11-13 06:18:01 -0800

    looping languges

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// a script that prints 3 lines using loop
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in args) {
+  console.log(args[i]);
+}
