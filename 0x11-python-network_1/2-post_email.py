771a8c852b4d8f3de39bd67d5a7b86673dc317d8
Ermi24et <lilermi24@gmail.com>
2023-12-02 00:25:15 +0300

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that sends a POST request to the passed URL with the email as a param
+a script that sends a POST request to the passed URL with the email
 """
 
 import urllib.request
