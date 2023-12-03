10f92078103dd04f600af39c8ef6ed8fb8c0f633
Ermi24et <lilermi24@gmail.com>
2023-12-04 02:31:03 +0300

    time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[1], sys.argv[2])
+        sys.argv[2], sys.argv[1])
 
     req = requests.get(url)
     commits = req.json()
