f12b743cf0ad2fd1f618a49765db1948e26e49bb
Ermi24et <lilermi24@gmail.com>
2024-01-03 16:18:26 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    console.log(data);
+  console.log(data);
 });
